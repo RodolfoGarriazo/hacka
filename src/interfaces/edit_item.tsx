879c8bff5f47ce06458a1d82559b7d5933bca59b
RodolfoGarriazo <rodolfo.garriazo@utec.edu.pe>
2024-11-09 15:51:47 -0500

    interface

---
+++ src/interfaces/edit_item.tsx
@@ -0,0 +1,10 @@
+export interface UpdateItem{
+    itemId:string;
+    boughtInLastMonth:number;
+    imgUrl:string;
+    isBestSeller:boolean;
+    price:number;
+    stars:number;
+    title: string;
+    
+}
---
+++ src/interfaces/item.tsx
@@ -0,0 +1,8 @@
+export interface Item{
+    boughtInLastMonth:number;
+    imgUrl:string;
+    isBestSeller:boolean;
+    price: number;
+    stars: number;
+    title: string;
+}
